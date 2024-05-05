32e1fc05aa5d49a5b5d485507a20fe3d2b44d973
Raphico <raphicogit@gmail.com>
2024-05-05 12:05:55 +0100

    style: prettier formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "tailwindcss"
-import {fontFamily} from "tailwindcss/defaultTheme"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -75,8 +75,8 @@ const config = {
       },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-        heading: ["var(--font-heading)", ...fontFamily.sans]
-    }
+        heading: ["var(--font-heading)", ...fontFamily.sans],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
