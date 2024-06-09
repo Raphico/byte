826d9de7a2d3586c37cf24d0f0ab4f96ccb86375
Raphico <raphicogit@gmail.com>
2024-06-09 15:24:39 +0100

    fix: update db path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { env } from "@/env"
 import type { Config } from "drizzle-kit"
 
 export default {
-  schema: "./src/lib/drizzle/schema.ts",
+  schema: "./src/server/db/schema.ts",
   out: "./drizzle/migrations",
   driver: "pg",
   dbCredentials: {
