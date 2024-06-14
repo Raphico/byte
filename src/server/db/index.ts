1d255d3e0447f9b21410959b2177767163302e66
Raphico <raphicogit@gmail.com>
2024-06-14 19:03:33 +0100

    chore: update db initialization

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,28 +1,8 @@
-/**
- *  The following code sets up a database connection using drizzle to postgres
- *  and prevents nextjs from creating multiple connections in development
- */
-
-import { env } from "@/env"
-import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js"
+import { env } from "@/env.js"
+import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
 import * as schema from "./schema"
 
-declare global {
-  // eslint-disable-next-line no-var -- only var works here
-  var db: PostgresJsDatabase<typeof schema> | undefined
-}
-
-let db: PostgresJsDatabase<typeof schema>
-
-if (process.env.NODE_ENV === "production") {
-  db = drizzle(postgres(env.DATABASE_URL), { schema })
-} else {
-  if (!global.db) {
-    global.db = drizzle(postgres(env.DATABASE_URL), { schema })
-  }
-  db = global.db
-}
-
-export { db }
+const client = postgres(env.DATABASE_URL)
+export const db = drizzle(client, { schema })
